cd1b52a76fcabaad6a54cb0953c9dca1a54e550f
Eduardo Gaytan <egaytan@webandando.com>
2020-12-06 22:17:20 -0600

    Finish Marcas

--- js/main.js
+++ js/main.js
@@ -1,22 +1,58 @@
 $('.events_carousel').slick({
     slidesToShow: 3,
     slidesToScroll: 1,
-    autoplay: false,
+    autoplay: true,
     arrows: false,
     autoplaySpeed: 2000,
     infinite: true,
     dots: true,
     responsive: [{
-            breakpoint: 768,
+            breakpoint: 992,
             settings: {
                 slidesToShow: 2
             }
         },
         {
-            breakpoint: 575,
+            breakpoint: 576,
             settings: {
                 slidesToShow: 1
             }
         }
+    ]
+});
+
+$('.aliados_carousel').slick({
+    dots: false,
+    arrows: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    responsive: [{
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 576,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
     ]
 });
